b85dcad7c7207b9e21d62c0d0020ad7d11fb1db4
Shivani Saini <52560286+shivani3495@users.noreply.github.com>
2021-08-26 13:22:46 +0530

    Add files via upload

---
+++ ml-model/linear mnist.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import re
+import boto3
+import sagemaker
+from sagemaker import get_execution_role
+
+sess = sagemaker.Session()
+
+region = boto3.Session().region_name
+
+# S3 bucket where the original mnist data is downloaded and stored.
+downloaded_data_bucket = f"sagemaker-sample-files"
+downloaded_data_prefix = "datasets/image/MNIST"
+
+# S3 bucket for saving code and model artifacts.
+# Feel free to specify a different bucket and prefix
+bucket = sess.default_bucket()
+prefix = "sagemaker/DEMO-linear-mnist"
+
+# Define IAM role
+role = get_execution_role()
+
+
+# In[2]:
+
+
+get_ipython().run_cell_magic('time', '', 'import pickle, gzip, numpy, json\n\n# Load the dataset\ns3 = boto3.client("s3")\ns3.download_file(downloaded_data_bucket, f"{downloaded_data_prefix}/mnist.pkl.gz", "mnist.pkl.gz")\nwith gzip.open("mnist.pkl.gz", "rb") as f:\n    train_set, valid_set, test_set = pickle.load(f, encoding="latin1")')
+
+
+# In[3]:
+
+
+get_ipython().magic('matplotlib inline')
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = (2, 10)
+
+
+def show_digit(img, caption="", subplot=None):
+    if subplot is None:
+        _, (subplot) = plt.subplots(1, 1)
+    imgr = img.reshape((28, 28))
+    subplot.axis("off")
+    subplot.imshow(imgr, cmap="gray")
+    plt.title(caption)
+
+
+show_digit(train_set[0][30], f"This is a {train_set[1][30]}")
+
+
+# In[4]:
+
+
+get_ipython().magic('matplotlib inline')
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = (2, 10)
+
+
+def show_digit(img, caption="", subplot=None):
+    if subplot is None:
+        _, (subplot) = plt.subplots(1, 1)
+    imgr = img.reshape((28, 28))
+    subplot.axis("off")
+    subplot.imshow(imgr, cmap="gray")
+    plt.title(caption)
+
+
+show_digit(train_set[0][20], f"This is a {train_set[1][20]}")
+
+
+# In[5]:
+
+
+import io
+import numpy as np
+import sagemaker.amazon.common as smac
+
+vectors = np.array([t.tolist() for t in train_set[0]]).astype("float32")
+labels = np.where(np.array([t.tolist() for t in train_set[1]]) == 0, 1, 0).astype("float32")
+
+buf = io.BytesIO()
+smac.write_numpy_to_dense_tensor(buf, vectors, labels)
+buf.seek(0)
+
+
+# In[6]:
+
+
+import boto3
+import os
+
+key = "recordio-pb-data"
+boto3.resource("s3").Bucket(bucket).Object(os.path.join(prefix, "train", key)).upload_fileobj(buf)
+s3_train_data = f"s3://{bucket}/{prefix}/train/{key}"
+print(f"uploaded training data location: {s3_train_data}")
+
+
+# In[7]:
+
+
+output_location = f"s3://{bucket}/{prefix}/output"
+print(f"training artifacts will be uploaded to: {output_location}")
+
+
+# In[8]:
+
+
+from sagemaker import image_uris
+
+container = image_uris.retrieve(region=boto3.Session().region_name, framework="linear-learner")
+
+
+# In[9]:
+
+
+import boto3
+import sagemaker
+
+sess = sagemaker.Session()
+
+linear = sagemaker.estimator.Estimator(
+    container,
+    role,
+    instance_count=1,
+    instance_type="ml.c4.xlarge",
+    output_path=output_location,
+    sagemaker_session=sess,
+)
+linear.set_hyperparameters(feature_dim=784, predictor_type="binary_classifier", mini_batch_size=200)
+
+linear.fit({"train": s3_train_data})
+
+
+# In[10]:
+
+
+linear_predictor = linear.deploy(initial_instance_count=1, instance_type="ml.m4.xlarge")
+
+
+# In[11]:
+
+
+from sagemaker.serializers import CSVSerializer
+from sagemaker.deserializers import JSONDeserializer
+
+linear_predictor.serializer = CSVSerializer()
+linear_predictor.deserializer = JSONDeserializer()
+
+
+# In[12]:
+
+
+result = linear_predictor.predict(train_set[0][30:31], initial_args={"ContentType": "text/csv"})
+print(result)
+
+
+# In[13]:
+
+
+import numpy as np
+
+predictions = []
+for array in np.array_split(test_set[0], 100):
+    result = linear_predictor.predict(array)
+    predictions += [r["predicted_label"] for r in result["predictions"]]
+
+predictions = np.array(predictions)
+
+
+# In[14]:
+
+
+import pandas as pd
+
+pd.crosstab(
+    np.where(test_set[1] == 0, 1, 0), predictions, rownames=["actuals"], colnames=["predictions"]
+)
+
+
+# In[ ]:
+
+
+
+
